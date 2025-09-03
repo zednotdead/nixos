91803cb6643b71bca10fc5066c2ce263cc04c141
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-09-03 19:23:35 +0200

    add exclusions to backup

--- home/zed/backup.nix
+++ home/zed/backup.nix
@@ -12,6 +12,11 @@ in {
         passwordFile = resticPassword;
         repository = "s3:http://10.0.2.3:9000/home-restic";
         exclude = builtins.map (x: homeDir + "/" + x) [
+	  ".bun"
+	  ".cargo"
+	  ".local/share/mise"
+	  ".local/share/pnpm"
+	  ".rustup"
           ".config/user-share"
           ".cache"
           ".local/share/Steam"
