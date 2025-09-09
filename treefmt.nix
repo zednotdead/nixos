9fcaec2f25dca17c0df273cf9f365c54b306d241
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-09-09 12:15:47 +0200

    add statix

--- treefmt.nix
+++ treefmt.nix
@@ -1,6 +1,12 @@
 # treefmt.nix
-{pkgs, ...}: {
+{...}: {
   projectRootFile = "flake.nix";
   programs.alejandra.enable = true;
   programs.taplo.enable = true;
+  programs.statix = {
+    enable = true;
+    disabled-lints = [
+      "empty_pattern"
+    ];
+  };
 }
