6d01c9fe6c4cfc8d0f902acdaa2e623ab8c5c497
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-09-13 20:32:23 +0200

    add ctrl-backspace to Fish

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -14,6 +14,7 @@
     kubectx
     exiftool
     comma
+    skim
   ];
 
   programs = {
@@ -40,7 +41,14 @@
       };
       shellInit = ''
         set fish_greeting
+        bind ctrl-h backward-kill-word
       '';
+      plugins = with pkgs.fishPlugins; [
+        {
+          name = "bangbang";
+          inherit (bang-bang) src;
+        }
+      ];
       shellAliases = {
         zel = "${pkgs.zellij}/bin/zellij";
         k = "${pkgs.kubectl}/bin/kubectl";
