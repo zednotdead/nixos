da22505228db9269b17ec96167f0dd79ab5c21f7
Zuzanna Żołnierowicz <zuzanna@zolnierowi.cz>
2025-12-10 21:36:20 +0100

    fix DMS

--- flake.nix
+++ flake.nix
@@ -38,10 +38,18 @@
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    dgop = {
+      url = "github:AvengeMedia/dgop";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.dgop.follows = "dgop";
     };
+
     vicinae = {
       # Programs
       url = "github:vicinaehq/vicinae";
--- modules/home/niri/shell.nix
+++ modules/home/niri/shell.nix
@@ -1,8 +1,5 @@
 {
-  perSystem,
   inputs,
-  config,
-  pkgs,
   ...
 }:
 {
