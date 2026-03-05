b8e7c196bc0c3f6a9854710f61f54b7d29f16f63
Zuzanna Żołnierowicz <zuzanna@zolnierowi.cz>
2026-03-05 14:15:43 +0100

    render html in email in chawan

--- modules/home/email/neomutt.nix
+++ modules/home/email/neomutt.nix
@@ -1,6 +1,20 @@
-{ lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [ mutt-ics ];
+  lib,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [mutt-ics monolith];
+
+  home.file.".mailcap" = {
+    enable = true;
+    text = ''
+      text/html;        ${pkgs.monolith}/bin/monolith -avj %s | cha -T text/html; nametemplate=%s.html; needsterminal;
+      text/calendar;    ${pkgs.khal}/bin/khal printics; copiousoutput;print=${pkgs.khal}/bin/khal import --batch
+      text/x-vcalendar; ${pkgs.khal}/bin/khal printics; copiousoutput;print=${pkgs.khal}/bin/khal import --batch
+      application/ics;  ${pkgs.khal}/bin/khal printics; copiousoutput;print=${pkgs.khal}/bin/khal import --batch
+    '';
+  };
+
   programs.neomutt = {
     enable = true;
     unmailboxes = true;
